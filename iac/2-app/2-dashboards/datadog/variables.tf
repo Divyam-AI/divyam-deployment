8e1fb9f05ac8d385c0886520cc1307ebed73a30b
mdharmarajan <mdharmarajan@gmail.com>
2026-06-29 12:06:10 +0530

    Fix bugs in kafka.json (extra comma) Add otel and superset rules Add variables for cluster name and environment Tighten clickhouse disk write rules to only get errors in last 10 minutes

--- iac/2-app/2-dashboards/datadog/variables.tf
+++ iac/2-app/2-dashboards/datadog/variables.tf
@@ -25,3 +25,13 @@ variable "datadog_app_key" {
   type        = string
   sensitive   = true
 }
+
+variable "cluster_name" {
+  description = "Target kube_cluster_name used to re-scope exported (prod) dashboards. Replaces the baked-in divyam-prod-k8s-cluster. From the root config (same source as 2-alerts)."
+  type        = string
+}
+
+variable "env" {
+  description = "Deployment env name (e.g. sandbox, preprod, prod). Re-scopes the baked-in `prod` env token in dashboard namespace tags (-prod- -> -<env>-) and metric-name prefixes (_prod_/_prod. -> _<env>_/_<env>.). On prod this is a no-op. From the root config."
+  type        = string
+}
